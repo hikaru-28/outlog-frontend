4d1bb7fcb9509c3638881660ededc3bb6a297edd
hikaru-28 <th_shine@icloud.com>
2026-05-05 14:28:52 +0900

    feat: ルーティング設定

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
-import { Button } from '@/components/ui/button'
+import { Routes, Route } from 'react-router-dom'
+import RegisterPage from './pages/RegisterPage.tsx'
+import LoginPage from './pages/LoginPage.tsx'
+import HomePage from './pages/HomePage.tsx'
+import InputNewPage from './pages/InputNewPage.tsx'
+import InputEditPage from './pages/InputEditPage.tsx'
+import OutputPage from './pages/OutputPage.tsx'
 
 function App() {
   return (
-    <div className="flex min-h-screen items-center justify-center">
-      <Button>shadcn動作確認</Button>
-    </div>
+    <Routes>
+      <Route path='/register' element={<RegisterPage />} />
+      <Route path='/login' element={<LoginPage />} />
+      <Route path='/' element={<HomePage />} />
+      <Route path='/inputs/new' element={<InputNewPage />} />
+      <Route path='/inputs/:id' element={<InputEditPage />} />
+      <Route path='/inputs/:id/output' element={<OutputPage />} />
+    </Routes>
   )
 }
 
