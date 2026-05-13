78861c41e1ef9d94ec3c15bfe9b1d0583e16fe32
hikaru-28 <th_shine@icloud.com>
2026-05-13 09:57:18 +0900

    chore: 環境変数管理追加

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://localhost:3000/api'
+const BASE_URL = import.meta.env.VITE_API_URL
 const fetchWithAuth = async (url: string, options?: RequestInit) => {
     const res = await fetch(url, {
         ...options,
