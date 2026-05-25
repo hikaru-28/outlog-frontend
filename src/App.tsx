f53937f0cbc3ed8a9e47fe2985ad4dd5273f52da
hikaru-28 <th_shine@icloud.com>
2026-05-25 21:29:48 +0900

    feat: StatsPageルーティング追加

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import RegisterPage from './pages/RegisterPage.tsx'
 import LoginPage from './pages/LoginPage.tsx'
 import PrivateRoute from './components/PrivateRoute.tsx'
 import HomePage from './pages/HomePage.tsx'
+import StatsPage from './pages/StatsPage.tsx'
 import InputNewPage from './pages/InputNewPage.tsx'
 import InputEditPage from './pages/InputEditPage.tsx'
 import OutputPage from './pages/OutputPage.tsx'
@@ -16,6 +17,7 @@ function App() {
       <Route path='/login' element={<LoginPage />} />
       <Route element={<PrivateRoute />} >
         <Route path='/home' element={<HomePage />} />
+        <Route path='/stats' element={<StatsPage />} />
         <Route path='/inputs/new' element={<InputNewPage />} />
         <Route path='/inputs/:id/edit' element={<InputEditPage />} />
         <Route path='/inputs/:id/output' element={<OutputPage />} />
